a09a7391dc0d2178cf3ca3cdb83555d9cceaba79
fangxlmr <fangxlmr@foxmail.com>
2019-04-12 22:24:04 +0800

    Add bash_aliases, make vimrc, plugins.vim visible.

--- autoconf.sh
+++ autoconf.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # This is an autoconfig shell script for configuring
 # personal developing environment, including Tmux, Vim.
 #
@@ -25,23 +25,21 @@ echo "[ok]"
 # Append bash aliases to .bash_aliases
 echo -n "=====> Checking .bash_aliases ..."
 if [ ! -f "$HOME/.bash_aliases" ]; then
-    touch $HOME/.bash_aliases
+    cp $PWD/bash_aliases $HOME/.bash_aliases
+else
+    nlines=$(wc -l <$PWD/bash_aliases)
+    diff $PWD/bash_aliases <(tail -n $nlines $HOME/.bash_aliases) > /dev/null
+    if [ $? -ne 0 ]; then
+        cat $PWD/bash_aliases >> $HOME/.bash_aliases
+    fi
 fi
-echo "[ok]"
-
-echo -n "=====> Appending tmux alias to .bash_aliases ..."
-echo ' # tmux config
-alias tmuxkill="tmux kill-session -t"
-alias tmuxattach="tmux a -t"
-alias tmuxnew="tmux new -s"
-alias tmuxls="tmux ls" ' >> $HOME/.bash_aliases
 echo "[ok]"
 
 echo -n "=====> Checking .vimrc ..."
 if [ -f $HOME/.vimrc ]; then
     mv $HOME/.vimrc $BACKUP_DIR
 fi
-cp $PWD/.vimrc $HOME
+cp $PWD/vimrc $HOME/.vimrc
 echo "[ok]"
 
 echo -n "=====> Checking .vim/ directory ..."
@@ -54,7 +52,7 @@ echo "=====> Configuring .plugins.vim ..."
 if [ -f $HOME/.vim/.plugins.vim ]; then
     rm $HOME/.vim/.plugins.vim
 fi
-cp $PWD/.plugins.vim $HOME/.vim/
+cp $PWD/plugins.vim $HOME/.vim/.plugins.vim
 echo "[ok]"
 
 # TODO May add some commands here to install plugins silently
---
+++ bash_aliases
@@ -0,0 +1,5 @@
+# Tmux config
+alias tmuxkill="tmux kill-session -t"
+alias tmuxattach="tmux a -t"
+alias tmuxnew="tmux new -s"
+alias tmuxls="tmux ls"
