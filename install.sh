fcdf50b6b06f949d5aeaf68105699926701ed586
fangxlmr <fangxlmr@foxmail.com>
2019-05-29 00:57:56 +0800

    Implement main installer
    Implement main install script for parsing command line, which
    will call install.sh in submodules to do actual installation work.

--- install.sh
+++ install.sh
@@ -1,123 +1,119 @@
 #!/bin/bash
-# This is an autoconfig shell script for configuring
-# personal developing environment, including Tmux, Vim.
+# This is an autoconfig shell script for configuring personal developing
+# environment, including Tmux, Vim.
 #
-# Note:
-#   1. root privilege is needed at first to install required packages.
-#   2. Some aliases are set fot tmux in .bash_aliases
-#   3. Old config are moved to $PWD/configs.backup/
+# Copyright (C) <2019>
+# Written by Fang Xiaoliang <fangxlmr@foxmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 
-PWD=`pwd`
-BACKUP_DIR="$PWD/configs.old/"
+AUTHOR="Fang Xiaoliang <fangxlmr@foxmail.com>"
+# Each module has a module name for logging.
+MODULE="MAIN"
+VERSION="0.0.2"
 
-echo "=====> Installing required packages ..." 
-sudo apt-get -y install -f vim ack tmux ctags cmake cscope\
-    build-essential cmake python3-dev clang
-echo "=====> Installing required packages ...[ok]" 
+# Each script has it's own absolute script directory.
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-echo -n "=====> Creating configs.old ..."
-if [[ ! -d $BACKUP_DIR ]]; then
-    mkdir $BACKUP_DIR
-fi
-echo "[ok]"
+# The following varialbles would be refered in subscripts.
+# Directory that all modules located in.
+export MODULES_DIR="$SCRIPT_DIR/modules"
+# User home directory.
+export HOME_DIR=$HOME
+# Backup directory to store old configs.
+export BACKUP_DIR="$SCRIPT_DIR/configs.old"
+# Some util functions.
+export UTILS_DIR="$SCRIPT_DIR/utils"
 
-# Append bash aliases to .bash_aliases
-echo -n "=====> Checking .bash_aliases ..."
-if [[ ! -f "$HOME/.bash_aliases" ]]; then
-    cp $PWD/bash_aliases $HOME/.bash_aliases
-else
-    nlines=$(wc -l <$PWD/bash_aliases)
-    diff $PWD/bash_aliases <(tail -n $nlines $HOME/.bash_aliases) > /dev/null
-    if [[ $? -ne 0 ]]; then
-        # Backup .bash_aliases
-        cp $HOME/.bash_aliases $BACKUP_DIR
-        cat $PWD/bash_aliases >> $HOME/.bash_aliases
-    # else
-        # Do nothing
+function usage {
+echo "./install.sh (OPTION)
+
+DESCRIPTION
+    Install configs for some software to provide a better user experience.
+
+    Mandatory arguments to long options are mandatory for short options too.
+
+    -A, --all
+        install all modules
+
+    --vim=LEVEL
+        install specified LEVEL config for Vim where LEVEL should be one of \"basic\", \"extened\" and \"advanced\"
+
+    --tmux
+        install config for Tmux
+
+    -h, --help
+        display this help and exit
+
+    -V, --version
+        output version information and exit"
+}
+
+function main {
+    # Option flags.
+    all=False
+    vim_level=False
+    tmux=False
+    zsh=False
+
+    # Set valid command line options.
+    if ! options=$(getopt -u -o AVh -l all,vim:,tmux,zsh,version,help  -- "$@"); then
+        # something went wrong, getopt will put out an error message for us
+        exit 1
     fi
-fi
-echo "[ok]"
+    set -- $options
 
-echo -n "=====> Checking .tmux.conf ..."
-if [[ ! -f $HOME/.tmux.conf ]]; then
-    cp $PWD/tmux.conf $HOME/.tmux.conf
-else
-    nlines=$(wc -l <$PWD/tmux.conf)
-    diff $PWD/tmux.conf <(tail -n $nlines $HOME/.tmux.conf) > /dev/null
-    if [[ $? -ne 0 ]]; then
-        # Backup .tmux.conf
-        cp $HOME/.tmux.conf $BACKUP_DIR
-        cat $PWD/tmux.conf >> $HOME/.tmux.conf
-    # else
-        # Do nothing
+    # Parsing arguments passed by command line.
+    while [ $# -gt 0 ]; do
+        case $1 in
+            -A|--all) all=True;;
+            -V|--version) echo "$VERSION";;
+            -h|--help) usage;;
+            --vim) vim_level="$2"; shift;;
+            --tmux) tmux=True;;
+            --zsh) zsh=True;;
+            (--) shift; break;;
+            (*) break;;
+        esac
+        shift
+    done
+
+    if [[ $all = "True" ]]; then
+        echo "[$MODULE] Installing all modules..."
+        # TODO: Calling install.sh in all modules.
+        echo "[$MODULE] Installing all modules...[ok]"
     fi
-fi
-echo "[ok]"
 
-echo -n "=====> Checking .vimrc ..."
-if [[ ! -f $HOME/.vimrc ]]; then
-    cp $PWD/vimrc $HOME/.vimrc
-else
-    diff $HOME/.vimrc $PWD/vimrc > /dev/null
-    if [[ $? -ne 0 ]]; then
-        mv $HOME/.vimrc $BACKUP_DIR
-        cp $PWD/vimrc $HOME/.vimrc
-    # else
-        # Do nothing
+    if [[ $vim_level != "False" ]]; then
+        echo "[$MODULE] Installing Vim..."
+        # TODO: Calling install.sh in Vim module.
+        echo "[$MODULE] Installing Vim...[ok]"
     fi
-fi
-echo "[ok]"
 
-echo -n "=====> Checking .vim/ directory ..."
-if [[ ! -d $HOME/.vim/ ]]; then
-    mkdir $HOME/.vim/
-fi
-echo "[ok]"
-
-echo -n "=====> Configuring .plugins.vim ..."
-if [[ ! -f $HOME/.vim/.plugins.vim ]]; then
-    cp $PWD/plugins.vim $HOME/.vim/.plugins.vim
-else
-    diff $HOME/.vim/.plugins.vim $PWD/plugins.vim > /dev/null
-    if [[ $? -ne 0 ]]; then
-        mv $HOME/.vim/.plugins.vim $BACKUP_DIR
-        cp $PWD/plugins.vim $HOME/.vim/.plugins.vim
-    # else
-        # Do nothing
+    if [[ $tmux = "True" ]]; then
+        echo "[$MODULE] Installing Tmux..."
+        # TODO: Calling install.sh in Tmux module.
+        echo "[$MODULE] Installing Tmux...[ok]"
     fi
-fi
-echo "[ok]"
 
-echo "=====> Installing Vundle ..."
-if [[ ! -d $HOME/.vim/bundle/Vundle.vim ]]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    echo "=====> Installing Vundle ...[ok]"
-else
-    echo "=====> Vundle is already installed ...[ok]"
-fi
+    if [[ $zsh = "True" ]]; then
+        echo "[$MODULE] Installing zsh..."
+        # TODO: Calling install.sh in zsh module.
+        echo "[$MODULE] Installing zsh...[ok]"
+    fi
+}
 
-echo "=====> Installing plugins for Vim ..."
-echo "This process will take a while, you can take a break ..."
-sleep 5
-vim tmpfile +PluginInstall +qall
-echo "=====> Installing plugins for Vim ...[ok]"
-
-echo "=====> Compiling stuff for YCM and Leaderf ..."
-echo "=====> [1] Compiling stuff for YCM ..."
-cd ~/.vim/bundle/YouCompleteMe
-if (./install.py --clang-completer); then
-    echo "=====> [1] Compiling stuff for YCM ...[ok]"
-else
-    echo "=====> [1] Something wrong happened when compiling YCM ...[ok]"
-fi
-
-echo "=====> [2] Compiling stuff for LeaderF ..."
-cd ~/.vim/bundle/LeaderF
-if (./install.sh); then
-    echo "=====> [2] Compiling stuff for LeaderF ...[ok]"
-else
-    echo "=====> [2] Something wrong happened when compiling LeaderF ...[ok]"
-fi
-echo "=====> Compiling stuff for YCM and Leaderf ...[ok]"
-
-echo "=====> Finish configuring you env. Enjoy! :-)"
+# Run main function finally to install modules.
+main "$@"
